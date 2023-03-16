0b5e1df907fe53bfca570f06438d5cfb4186771d
Muller Jimmy <JimmyMullerGuirdin@gmail.com>
2023-03-16 15:35:53 +0100

    init

---
+++ src/client.cpp
@@ -0,0 +1,59 @@
+// #include "../lib/json-develop/include/nlohmann/json.hpp"
+#include <nlohmann/json.hpp>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using std::string;
+using json = nlohmann::json;
+
+string username = "root";
+string password = "root";
+string idUser = "1";
+
+bool connexionBDD(string name, string pass){
+
+    if(username==name && password==pass){
+        std::cout << "Login success" << std::endl;
+        return true;
+
+    }
+    else{
+        std::cout << "Login failed" << std::endl;
+        return false;
+    }
+    
+}
+
+void createJsonFile(){
+    std::cout << "Create json file :" << std::endl;
+    std::ifstream file("../data/"+idUser+".json");
+}
+
+int main(){
+    
+    string name;
+    string pass;
+
+    // Connexion
+    std::cout << "Username:" << std::endl;
+    std::cin >> name;
+
+    std::cout << "Password:" << std::endl;
+    std::cin >> pass;
+
+    if(connexionBDD(name, pass)){
+        createJsonFile();
+
+
+
+    }
+    
+    return 0;
+}
+// Database 
+
+// Tables
+
+
+// std::ofstream file(name+".json");
